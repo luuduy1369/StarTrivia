3055797e134ba7b7f436c4c9ace85ccdaed03392
luuduy1369 <khanhduyluu@gmail.com>
2019-06-04 09:26:40 +0700

    Implement:  - Using Alamofire for web request method  - Parsing json data by using JSONSwifty and Codable

---
+++ StarTrivia/Model/Person.swift
@@ -0,0 +1,35 @@
+//
+//  Person.swift
+//  StarTrivia
+//
+//  Created by pro on 6/3/19.
+//  Copyright © 2019 LUUKHANHDUY. All rights reserved.
+//
+
+import Foundation
+
+struct Person: Codable {
+    let name: String
+    let height: String
+    let mass: String
+    let hair: String
+    let birthYear: String
+    let gender: String
+    let homeworldUrl: String
+    let filmUrls: [String]
+    let vehicleUrls: [String]
+    let starshipUrls: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case height
+        case mass
+        case hair = "hair_color"
+        case birthYear = "birth_year"
+        case gender
+        case homeworldUrl = "homeworld"
+        case filmUrls = "films"
+        case vehicleUrls = "vehicles"
+        case starshipUrls = "starships"
+    }
+}
